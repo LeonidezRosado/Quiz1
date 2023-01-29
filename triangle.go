5503277a212ce6825d8bfdc15b577ce979395223
Leonidez A. Rosado <2020152397@ub.edu.bz>
2023-01-28 18:04:33 -0600

    question2

--- triangle.go
+++ triangle.go
@@ -6,12 +6,16 @@ import (
 )
 
 
-//creating a type called "triangle" with two fields named base and height (type = float64)
+//1)creating a type called "triangle" with two fields named base and height (type = float64)
 type triangle struct{
 	base float64
 	height float64
 }
 
+//2)creating a method on type triangle named area that calculates and returns area 
+func (t triangle) Area() float64{
+	return 1/2 * (t.base * t.height)
+}
 
 func main() {
 
