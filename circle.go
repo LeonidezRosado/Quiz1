c792ff604339d8fde205b59f917999c7b3214f4f
Leonidez A. Rosado <2020152397@ub.edu.bz>
2023-01-28 19:30:18 -0600

    question7

--- circle.go
+++ circle.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-//creating a type named circle that has one field named 'radius' of type float64
+//6)creating a type named circle that has one field named 'radius' of type float64
 type circle struct{
 	radius float64
+}
+
+//7)creating a method on type circle named "area" that calculates and returns area 
+func (c circle) area() float64 {
+	var y float64 = 2
+	return  math.Pi * (math.Pow(c.radius,y))
 }
 
 func main() {
