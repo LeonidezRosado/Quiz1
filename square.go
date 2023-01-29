456abee356809a984e5a6007646fbcaa53a7ca07
Leonidez A. Rosado <2020152397@ub.edu.bz>
2023-01-29 17:41:18 -0600

    question10

--- square.go
+++ square.go
@@ -5,7 +5,7 @@ import (
 )
 
 //9)creating a go function named square that accepted 
-func square(side float64) (float64, float64) {
+func Square(side float64) (float64, float64) {
 	var y float64 = 2
 	return math.Pow(side,y), side + side + side + side
 }
--- square_test.go
+++ square_test.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestSquare(t *testing.T) {
+	got1,got2 := Square(10)
+	expected1, expected2 := 100,40
+	if got1  != float64(expected1) && got2 != float64(expected2) {
+		t.Errorf("got %v expected %v, \n got%v expected %v", got1,expected1,got2,expected2)
+	}
+}
