e124f9e357c762e22471b4a88abb83a1f1a6191d
Leonidez A. Rosado <2020152397@ub.edu.bz>
2023-01-28 19:16:42 -0600

    question6

--- circle.go
+++ circle.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+)
+
+//creating a type named circle that has one field named 'radius' of type float64
+type circle struct{
+	radius float64
+}
+
+func main() {
+
+}
